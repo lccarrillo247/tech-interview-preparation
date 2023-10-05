4a663a1b6373e7ff169bedc6787b7b84055bc0f6
lccarrillo247 <lccarrillo247@gmail.com>
2023-10-05 08:57:29 -0500

    09-Curry

--- Ejercicios/09-Curry/Curry.js
+++ Ejercicios/09-Curry/Curry.js
@@ -1,6 +1,16 @@
 function curry (fn) {
   // Your code here:
+const args = [];
 
+const argsNum = fn.length;
+
+return function curried(arg) {
+  if (args.length !== argsNum) {
+    return args.push(arg);
+  } else {
+  fn(...args);
+  }
+}
 }
 
 module.exports = curry
